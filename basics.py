2a2072883056dbebc22dc856e074edff44579cb7
rohan_rt15 <rohan_rt15@yahoo.in>
2021-01-24 01:31:39 +0530

    basics.py file created

---
+++ basics.py
@@ -0,0 +1,12 @@
+#first python program
+
+roh = "Hello Rohan!"
+print(roh)
+
+# passing variable in print statement
+
+a = "Hello"
+b = "Python"
+print(a + " " + b)
+
+print(roh + " and " + a + " " + b)
